3f4e80120014a8b7230918665dc9348f8526478a
dlwogmlzz <lwwljh90@gmail.com>
2024-02-11 20:37:20 +0900

    Java입문 - Variable

---
+++ java-start/src/variable/Var3.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        a = 30; // 변수 초기화 a(30)
+        System.out.println(a);
+        a = 40; // 변수 초기화 a(30 -> 40)
+        System.out.println(a);
+
+    }
+}
---
+++ java-start/src/variable/ex/VarEx1.java
@@ -0,0 +1,15 @@
+package variable.ex;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
